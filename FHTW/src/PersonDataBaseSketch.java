4698034df63a2349f7c8392d86517a6480c3eb4a
hganibeg <ganibegovic8@gmail.com>
2023-12-12 13:17:14 +0100

    PersonDataBaseSketch first push

---
+++ FHTW/src/PersonDataBaseSketch.java
@@ -0,0 +1,19 @@
+public class PersonDataBaseSketch {
+    public static void main(String[] args) {
+        long[] pUIDs = {1,2,3};
+        String[] VN ={"Jim", "Jane", "John"};
+        String[] NN ={"Doe", "Deer", "Jackson"};
+        int[] dobD={1,3,15};
+        int[] dobM={9,11,1};
+        int[] dobY={1959,2010,1987};
+
+        printDB(pUIDs,VN,NN,dobD,dobM,dobY);
+
+    }
+
+    public static void printDB (long[] uid, String[] VN, String[] NN, int[] d, int[] m, int[] y) {
+        for (int i = 0; i < uid.length; i++) {
+            System.out.printf("%05d %-10.10S %-10.10S %04d-%02d-%02d\n",uid[i], VN[i], NN[i], d[i], m[i], y[i]);
+        }
+    }
+}
