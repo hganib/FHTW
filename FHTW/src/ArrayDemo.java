78c0893df6046359c39179dd05ef79324eef78aa
hganibeg <ganibegovic8@gmail.com>
2023-12-12 12:01:06 +0100

    Test Commit 11:50

---
+++ FHTW/src/ArrayDemo.java
@@ -0,0 +1,8 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] a_array={1,2,3,4,5,6};
+        for (int i = 0; i < a_array.length; i++) {
+            System.out.printf("%d",a_array[i]);
+        }
+    }
+}
